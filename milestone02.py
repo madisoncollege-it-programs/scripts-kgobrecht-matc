ed671db39efd3d627c1860de26fb5950ce2221fa
kgobrecht-matc <kgobrecht@madisoncollege.edu>
2026-02-18 19:00:51 -0600

    milestone02.py is ready for Grading.

---
+++ milestone02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Author: Keaton Gobrecht
+Email: kgobrecht@madisoncollege.edu
+Description: <Semester long script which analyzes an Apache web log to determine if the highest-hitting IP address is a current threat.>
+"""
+print("Keaton Gobrecht")
+
+strUserInputName = input("What is your name?\n>>>> ")
+print()
+print("Welcome, " , {strUserInputName} , "!")
---
+++ myScriptTemplate.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+Author: Keaton Gobrecht
+Email: kgobrecht@madisoncollege.edu
+Description: <My First Script>
+"""
+print("Keaton Gobrecht")
