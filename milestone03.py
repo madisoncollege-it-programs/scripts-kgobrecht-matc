55ffffa4620558e13f92568573d7d3685a830ffa
kgobrecht-matc <kgobrecht@madisoncollege.edu>
2026-03-11 19:21:32 -0500

    Milestone03 Ready for Grading

--- milestone03.py
+++ milestone03.py
@@ -5,11 +5,12 @@ Email: kgobrecht@madisoncollege.edu
 Description: <Semester long script which analyzes an Apache web log to determine if the highest-hitting IP address is a current threat.>
 """
 print("Keaton Gobrecht")
+strUserInput = input("What is your name?\n >>>> ")
+print(f"Welcome, {strUserInput}!")
 
-strUserInputName = input("What is your name?\n>>>> ")
-print()
-print("Welcome, " , {strUserInputName} , "!")
+#Single log line from the Apache Log. This will eventually be replaced.
 strLogLine = '111.222.333.123 HOME - [01/Feb/1998:01:08:39 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 198 "http://www.referrer.com/bannerad/ba_intro.htm" "Mozilla/4.01 (Macintosh; I; PPC)"'
-listLogLine = strLogLine.split()
-print(f"log request from:  {listLogLine [0]:*^22s}")
-print(f"Return Code: {listLogLine[8]}")
+
+listLogline = strLogLine.split(" ")
+print(f"Log request from:  {listLogline[0]:*^22}")
+print(f"Return Code: {listLogline[8]}")
